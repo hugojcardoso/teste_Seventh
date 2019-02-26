fa4cd77fb4bc07a8c749e9381fca8a05eac5761d
hugojcardoso <hjc.cad@gmail.com>
2019-02-26 13:10:54 -0300

    Correção do controller para uso da API

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -9,73 +9,70 @@ angular.module("meuModulo")
 	$scope.titulo = "Carrinho";
 })
 
-.controller("adminController",function($scope){
+.controller("adminController",function($scope, $http){
 	$scope.titulo = "Admin";
-	$scope.produtos = [
-		{nome: "Leite",fabricante:"Tirol",custo:65,venda:80,quantidade:55},
-		{nome: "Nescau",fabricante:"Nestlé",custo:75,venda:80,quantidade:55},
-		{nome: "Arroz",fabricante:"Tio João",custo:65,venda:60,quantidade:55},
-		{nome: "Feijão",fabricante:"Tio João",custo:95,venda:80,quantidade:55},
-		{nome: "Farinha",fabricante:"Roça seca",custo:65,venda:30,quantidade:55}
-	];
+	
+	$http.get('https://s-shop-test.herokuapp.com/product').success(function(data, status, headers, config){
+		
+		$scope.products = data;
+		
+	}).error(function(data, status, headers,config){
 
-	var init = function(){
-		$scope.produtos.forEach(function(produto){
-			produto.media = media(produto);
-		});
-		limpaForm();
-	};
+	});
 
-	var media = function(Produto){		
-		var media = (parseFloat(Produto.custo) + parseFloat(Produto.venda) + parseFloat(Produto.quantidade))/3;
-		return media.toFixed(2);
-	};
+	// var init = function(){
+	// 	$scope.produtos.forEach(function(produto){
+			
+	// 	});
+	// 	limpaForm();
+	// };
 
-	$scope.abreAddProduto = function(){
-		$scope.editando = false;
-		limpaForm();
-		$('#modal1').openModal();
-	};
+	
 
-	$scope.addProduto = function(Produto){
-		Produto.media = media(Produto);
-		$scope.produtos.push(Produto);
-		$('#modal1').closeModal();
-		limpaForm();
-	};
+	// $scope.abreAddProduto = function(){
+	// 	$scope.editando = false;
+	// 	limpaForm();
+	// 	$('#modal1').openModal();
+	// };
 
-	$scope.editando = false;
-	var produtoEditar;
+	// $scope.addProduto = function(Produto){
+	// 	$scope.produtos.push(Produto);
+	// 	$('#modal1').closeModal();
+	// 	limpaForm();
+	// };
 
-	$scope.editarProduto = function(Produto){
-		$scope.editando = true;			
-		angular.copy(Produto,$scope.Produto);
-		$('#modal1').openModal();
-		produtoEditar = Produto;
-	};
+	// $scope.editando = false;
+	// var produtoEditar;
 
-	$scope.salvarProduto = function(Produto){
-		produtoEditar.nome = Produto.nome;
-		produtoEditar.fabricante = Produto.fabricante;
-		produtoEditar.custo = Produto.custo;
-		produtoEditar.venda = Produto.venda;
-		produtoEditar.quantidade = Produto.quantidade;
-		produtoEditar.media = media(Produto);
-		$('#modal1').closeModal();
-	};
+	// $scope.editarProduto = function(Produto){
+	// 	$scope.editando = true;			
+	// 	angular.copy(Produto,$scope.Produto);
+	// 	$('#modal1').openModal();
+	// 	produtoEditar = Produto;
+	// };
 
-	$scope.deletarProduto = function(Produto){
-		for(var index in $scope.produtos){
-			var aux = $scope.produtos[index];
-			if(Produto === aux){
-				$scope.produtos.splice(index,1);
-			}
-		}
-	};
+	// $scope.salvarProduto = function(Produto){
+	// 	produtoEditar.nome = Produto.nome;
+	// 	produtoEditar.fabricante = Produto.fabricante;
+	// 	produtoEditar.custo = Produto.custo;
+	// 	produtoEditar.venda = Produto.venda;
+	// 	produtoEditar.quantidade = Produto.quantidade;
+	// 	$('#modal1').closeModal();
+	// };
 
-	var limpaForm = function(){
-		$scope.Produto = {nome: "",fabricante:"",custo:'',venda:'',quantidade:'',media:''};
-	};
+	// $scope.deletarProduto = function(Produto){
+	// 	for(var index in $scope.produtos){
+	// 		var aux = $scope.produtos[index];
+	// 		if(Produto === aux){
+	// 			$scope.produtos.splice(index,1);
+	// 		}
+	// 	}
+	// };
 
-	init();
+	// var limpaForm = function(){
+	// 	$scope.Produto = {nome: "",fabricante:"",custo:'',venda:'',quantidade:''};
+	// };
+
+
+	//init();
 })
